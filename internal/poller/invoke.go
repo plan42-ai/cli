4e0d102dfabe01cb17f2e85a8643f5ba42fdd253
Plan42.ai <robot@plan42.ai>
2026-01-27 21:24:30 +0000

    Fix UI timeouts when creating tasks / turns with remote runners.
    1. We were fetching PR feedback and pulling container images before acking the message in the runner. This would lead to timeouts and displaying errors in the UI.
    2. We now update the status of the turn when fetchign PR feedback and when pulling the container image so that the UI doesn't just show "pending".

--- internal/poller/invoke.go
+++ internal/poller/invoke.go
@@ -1,10 +1,12 @@
 package poller
 
 import (
+	"github.com/plan42-ai/sdk-go/p42"
 	"github.com/plan42-ai/sdk-go/p42/messages"
 )
 
 type pollerInvokeAgentRequest struct {
 	InvokePlatformFields
 	messages.InvokeAgentRequest
+	client *p42.Client
 }
